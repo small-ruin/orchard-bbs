e7afccac00c4241f15be95c81e84d446272af004
hilshire <hillshire.triela@gmail.com>
2021-03-17 16:51:48 +0800

    分割app.ts

---
+++ components/Section.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent, ReactChild } from 'react';
+import { useColorScheme,View, Text, StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
+interface Props {
+  children: Element,
+  title: string,
+}
+
+export const Section: FunctionComponent<Props> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+});
+
+export default Section;
